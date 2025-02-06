f094c7cd796d75ecba63201bcad0809bc628969c
Sean Kipinä <paintballsean@gmail.com>
2025-02-07 00:25:28 +0200

    Only one test is failing

--- art-decoder/functions/bracket_validation.go
+++ art-decoder/functions/bracket_validation.go
@@ -4,27 +4,42 @@ import (
 	"errors"
 	"regexp"
 	"strconv"
+	"strings"
 )
 
 // ValidateBrackets ensures the encoded string has correctly balanced brackets.
 func ValidateBrackets(input string) error {
 	stack := 0
-	for _, char := range input { // Removed unused 'i'
+	firstOpenIndex := strings.Index(input, "[")
+	firstCloseIndex := strings.Index(input, "]")
+
+	// Detect misplaced closing bracket at the wrong position
+	if firstCloseIndex != -1 && (firstOpenIndex == -1 || firstCloseIndex < firstOpenIndex) {
+		return errors.New("Error: Missing opening bracket")
+	}
+
+	for i, char := range input {
 		if char == '[' {
 			stack++
 		} else if char == ']' {
 			if stack == 0 {
+				// Check if this `]` is in a position where a `[` should have been
+				if i == 0 || input[i-1] != ']' {
+					return errors.New("Error: Missing opening bracket")
+				}
 				return errors.New("Error: Extra closing bracket found")
 			}
 			stack--
 		}
 	}
+
+	// If there are unmatched opening brackets, return "Missing closing bracket"
 	if stack > 0 {
 		return errors.New("Error: Missing closing bracket")
 	}
+
 	return nil
 }
-
 // ValidateArguments checks if the arguments inside square brackets are valid.
 func ValidateArguments(input string) error {
 	pattern := regexp.MustCompile(`$begin:math:display$\\s*(\\d+)\\s+([^][]*)\\s*$end:math:display$`)
