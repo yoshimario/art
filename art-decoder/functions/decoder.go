4166674eda4f068834890418c2555fb2eb34b819
Sean Kipinä <paintballsean@gmail.com>
2025-02-15 14:23:30 +0200

    Combined multi and sinlge line functions to one decoder.go file

--- art-decoder/functions/decoder.go
+++ art-decoder/functions/decoder.go
@@ -1,10 +1,26 @@
 package functions
 
 import (
-    "errors"
-    "strconv"
-    "strings"
+	"errors"
+	"strconv"
+	"strings"
 )
+
+// DecodeMultiLine processes multiple lines of encoded input.
+func DecodeMultiLine(encodedString string) (string, error) {
+	lines := strings.Split(encodedString, "\n")
+	var result strings.Builder
+
+	for _, line := range lines {
+		decoded, err := DecodeSingleLine(line)
+		if err != nil {
+			return "", err
+		}
+		result.WriteString(decoded + "\n")
+	}
+
+	return strings.TrimRight(result.String(), "\n"), nil
+}
 
 func DecodeSingleLine(encoded string) (string, error) {
     // 1) Validate bracket structure/format first
